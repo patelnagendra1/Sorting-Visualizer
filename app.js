2bae12b41dfc77e39fe77794d0a204fb7bdcf7a6
Shlok Panpaliya <shlokp762@gmail.com>
2020-04-19 17:44:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
+app.use(express.static("public"));
 app.get("/", function(req, res) {
-    res.render("sort")
+    res.render("sort");
 });
-app.post("/",function(req,res){
-
-})
+// app.post("/",function(req,res){
+//
+// })
 
 app.listen(3000, function() {
   console.log("Live server is set up at port 3000");
