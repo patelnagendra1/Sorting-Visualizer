7ea87ae8e0e6982df21c3becb6b1d76a30561d54
patelnagendra1 <55997071+patelnagendra1@users.noreply.github.com>
2020-04-22 09:53:15 +0530

    Update app.js
    added function name and displayed the output with function name

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
+ function quick()
+{
+  var userInput = document.getElementById("userInput").value;
+            document.write(userInput+"=> quick");
+}
+
+function bubble()
+{
+  var userInput = document.getElementById("userInput").value;
+            document.write(userInput + "=> bubble");
+}
+
+function merge()
+{
+  var userInput = document.getElementById("userInput").value;
+            document.write(userInput + "=> merge");
+}
+
+function insertion()
+{
+  var userInput = document.getElementById("userInput").value;
+            document.write(userInput + " =>insertion");
+}
+
+function selection()
+{
+  var userInput = document.getElementById("userInput").value;
+            document.write(userInput + " =>selection");
+}
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
+
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -16,4 +46,6 @@ app.get("/", function(req, res) {
 
 app.listen(3000, function() {
   console.log("Live server is set up at port 3000");
+  
 });
+
