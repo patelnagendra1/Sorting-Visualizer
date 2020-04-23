d73c96c9a44e765448d991077136dc35a5a79465
Shlok Panpaliya <shlokp762@gmail.com>
2020-04-23 16:25:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,11 +10,37 @@ app.use(express.static("public"));
 app.get("/", function(req, res) {
     res.render("sort");
 });
+
+function bubble_Sort(arr){
+    var len = arr.length,
+        i, j, stop;
+
+    for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+            if (arr[j] > arr[j+1]){
+                let t = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = t;
+            }
+        }
+    }
+  }
+
+
  app.post("/",function(req,res){
-   console.log(req.body);
+
+  // console.log(req.body);
+   let x = parseInt(req.body.size);
+  // console.log(x);
    if(req.body.bubble === 'bubble')
    {
-      res.send("Bubble Sort execeuted succesfiully");
+
+      let arr = (Array.from({length: x}, () => Math.floor(Math.random() * x)));
+      //console.log(arr);
+      var start = new Date().getTime();
+      bubble_Sort(arr);
+      var end = new Date().getTime();
+      res.send("Bubble Sort execeuted succesfiully Time req was "+ (end-start) + " millisecond");
    }
    else if (req.body.quick === 'quick') {
      res.send("Quick Sort execeuted succesfiully");
